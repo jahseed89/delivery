912681b2f84a346e543c8e29619122da1033b336
Samson Ocran <samson.ocran@gmail.com>
2023-12-08 21:11:09 +0100

    animation loaded

---
+++ script.js
@@ -0,0 +1,52 @@
+// gsap.from(
+//     ".nav-logo-anim, .nav__items>ul>li, .btn-outline-custom-primary, .btn-outline-custom-secondery",
+//     { opacity: 0, duration: 0.5, y: -50 }
+//   );
+//   gsap.to(".about-first-move", { opacity: 1, duration: 1, y: -60 });
+
+//   gsap.to(".about-second-move", {
+//     opacity: 1,
+//     duration: 1.5,
+//     y: -50,
+//   });
+
+//   gsap.from(".delevery__hero-img", {
+//     scale: 0,
+//     duration: 2,
+//     ease: "easeOut",
+//   });
+
+//   gsap.from(".burga-img, .delevery-time-img", {
+//     scale: -1,
+//     duration: 2.5,
+//     delay: 1.1,
+//     ease: "easeOut",
+//   });
+
+//   gsap.to(".rate-first-move", {
+//     duration: 0.5,
+//     x: -50,
+//     ease: "power2.inOut",
+//   });
+//   gsap.to(".rate-second-move", {
+//     duration: 0.8,
+//     x: -50,
+//     ease: "power2.inOut",
+//   });
+//   gsap.to(".rate-third-move", {
+//     duration: 1,
+//     x: -50,
+//     ease: "power2.inOut",
+//   });
+//   gsap.to(".rate-forth-move", {
+//     duration: 1.3,
+//     x: -50,
+//     ease: "power2.inOut",
+//   });
+
+//   gsap.from(".about-us__hero", {
+//     scale: 0,
+//     duration: 1,
+//     ease: "easeOut",
+//   });
+  
